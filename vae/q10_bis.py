da5b927196537eb8fed85d19ca42d0bb44d6fa72
Alexandre Perez <33580936+alexprz@users.noreply.github.com>
2021-01-22 12:29:46 +0100

    Q10_b OK

--- vae/q10_bis.py
+++ vae/q10_bis.py
@@ -16,70 +16,36 @@ def imshow(img):
     plt.imshow(np.transpose(img.detach().numpy(), (1, 2, 0)))
 
 
-
 vae = VAE()
 state_dict = torch.load('trained_models/vae-epochs_80-lr_0.01-bs_128-gclip_1.pth')
 vae.load_state_dict(state_dict)
-# vae.load_state_dict(state_dict)
 
 os.makedirs('figs/q10_bis/', exist_ok=True)
 mnist = datasets.MNIST('MNIST/', train=True, download=True, transform=transforms.ToTensor())
 loader = DataLoader(mnist, batch_size=2, shuffle=False)
 
-# Ground truth
-x, target = next(iter(loader))
-# print(x.size())
-x1, x2 = torch.split(x, split_size_or_sections=1, dim=0)
-# print(x1.size())
-# print(x2.size())
-z1 = vae.encoder(x1)[0]
-z2 = vae.encoder(x2)[0]
-# print(z1.size())
-# print(z2.size())
-N = 8
-Z = [alpha*z1 + (1-alpha)*z2 for alpha in np.linspace(0, 1, N)]
-Z = torch.cat(Z, dim=0)
+iterator = iter(loader)
+Xs = []
 
-print(Z.shape)
+N = 16  # Number of samples in the linear interpolation
+M = 8  # Number of interpolations
+for _ in range(M):
+    x, target = next(iterator)
+    x1, x2 = torch.split(x, split_size_or_sections=1, dim=0)
 
-X = vae.decoder(Z)
+    z1 = vae.encoder(x1)[0]
+    z2 = vae.encoder(x2)[0]
 
-print(X.shape)
+    Z = [alpha*z1 + (1-alpha)*z2 for alpha in np.linspace(0, 1, N)]
+    Z = torch.cat(Z, dim=0)
 
-imshow(make_grid(X))
+    X = vae.decoder(Z)
+
+    Xs.append(X)
+
+Xs = torch.cat(Xs, dim=0)
+
+imshow(make_grid(Xs, nrow=N))
 plt.axis('off')
-plt.savefig('figs/q10_bis/mnist-vae.pdf', bbox_inches='tight')
-plt.show()
-
-exit()
-imshow(make_grid(x))
-plt.axis('off')
-plt.savefig('figs/mnist-ground-turth.pdf', bbox_inches='tight')
-plt.show()
-exit()
-
-# Without training
-x_reconstruct = vae.reconstruct(x)
-imshow(make_grid(x_reconstruct))
-plt.axis('off')
-plt.savefig('figs/mnist-vae-no-training.pdf', bbox_inches='tight')
-
-# With training (10 epochs)
-state_dict = torch.load('trained_models/vae-epochs_10-lr_0.001-bs_128-gclip_1.pth')
-vae.load_state_dict(state_dict)
-
-x_reconstruct = vae.reconstruct(x)
-imshow(make_grid(x_reconstruct))
-plt.axis('off')
-plt.savefig('figs/mnist-vae-10-epochs.pdf', bbox_inches='tight')
-
-# With training (80 epochs)
-state_dict = torch.load('trained_models/vae-epochs_80-lr_0.01-bs_128-gclip_1.pth')
-vae.load_state_dict(state_dict)
-
-x_reconstruct = vae.reconstruct(x)
-imshow(make_grid(x_reconstruct))
-plt.axis('off')
-plt.savefig('figs/mnist-vae-80-epochs.pdf', bbox_inches='tight')
-
+plt.savefig(f'figs/q10_bis/mnist-vae-N_{N}-M_{M}.pdf', bbox_inches='tight')
 plt.show()
