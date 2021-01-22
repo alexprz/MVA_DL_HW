ece43c8a8df71d69e60a8d1616f6274f75e58232
Alexandre Perez <33580936+alexprz@users.noreply.github.com>
2021-01-22 13:33:12 +0100

    new structure

--- vae/unet_vae.py
+++ vae/unet_vae.py
@@ -19,46 +19,42 @@ class UNetVAE(nn.Module):
         super().__init__()
 
         self.encode = nn.Sequential(  # 28, 28
-            nn.Conv2d(1, 64, kernel_size=3),  # 26, 26
+            nn.Conv2d(1, 10, kernel_size=3),  # 26, 26
             nn.ReLU(),
-            nn.Conv2d(64, 64, kernel_size=3),  # 24, 24
+            nn.MaxPool2d(2),  # 13, 13
+            nn.Conv2d(10, 20, kernel_size=3),  # 11, 11
             nn.ReLU(),
-            nn.MaxPool2d(2),  # 12, 12
-            nn.Conv2d(64, 128, kernel_size=3),  # 10, 10
+            nn.MaxPool2d(2),  # 6, 6
+            nn.Conv2d(20, 40, kernel_size=3),  # 4, 4
             nn.ReLU(),
-            nn.Conv2d(128, 128, kernel_size=3),  # 8, 8
-            nn.ReLU(),
-            nn.MaxPool2d(2),  # 4, 4
-            nn.Conv2d(128, 8, kernel_size=3),  # 2, 2
-            nn.ReLU(),
+            nn.MaxPool2d(2),  # 2, 2
+            # nn.Conv2d(30, 40, kernel_size=3),  # 1, 1
+            # nn.ReLU(),
         )
 
-        self.decode = nn.Sequential(  # 4, 4  (1 channel)
-            nn.Upsample(scale_factor=2),  # 8, 8
-            nn.Conv2d(1, 128, kernel_size=3),  # 6, 6
-            nn.ReLU(),
-            nn.Upsample(scale_factor=2),  # 12, 12
-            nn.Conv2d(128, 64, kernel_size=3),  # 10, 10
+        self.decode = nn.Sequential(  # 1, 1
+            nn.Upsample(scale_factor=12),  # 12, 12
+            nn.Conv2d(20, 15, kernel_size=3),  # 10, 10
             nn.ReLU(),
             nn.Upsample(scale_factor=2),  # 20, 20
-            nn.Conv2d(64, 32, kernel_size=3),  # 18, 18
-            nn.ReLU(),
-            nn.Conv2d(32, 32, kernel_size=3),  # 16, 16
+            nn.Conv2d(15, 10, kernel_size=5),  # 16, 16
             nn.ReLU(),
             nn.Upsample(scale_factor=2),  # 32, 32
-            nn.Conv2d(32, 16, kernel_size=3),  # 30, 30
-            nn.ReLU(),
-            nn.Conv2d(16, 1, kernel_size=3),  # 28, 28
+            nn.Conv2d(10, 1, kernel_size=5),  # 28, 28
             nn.Sigmoid(),
         )
 
     def encoder(self, x):
         """Implement the encoder part of the network."""
-        x = self.encode(x)  # 2, 2 (8 channels)
-        mu, logvar = torch.split(x, split_size_or_sections=4, dim=1)
+        x = self.encode(x)  # 1, 1
+        mu, logvar = torch.split(x, split_size_or_sections=20, dim=1)
 
-        mu = mu.view(-1, 2*2*4)  # 16
-        logvar = logvar.view(-1, 2*2*4)  # 16
+        # print(mu.size())
+        # print(logvar.size())
+        # exit()
+
+        mu = mu.view(-1, 20)  # 20
+        logvar = logvar.view(-1, 20)  # 20
 
         return mu, logvar
 
@@ -80,7 +76,7 @@ class UNetVAE(nn.Module):
         # print(mu.size())
         # print(logvar.size())
         # print(z.size())
-        z = z.view(-1, 1, 4, 4)
+        z = z.view(-1, 20, 1, 1)
         # print(z.size())
         # exit()
         # Decode the sampled z
@@ -108,7 +104,7 @@ if __name__ == '__main__':
 
     # Training parameters
     batch_size = 128
-    lr = 0.0001
+    lr = 0.001
     n_epochs = 80
     gclip = 1
     save_points = [1, 5, 10, 20, 30, 40, 50, 60, 70, 80]
@@ -122,7 +118,7 @@ if __name__ == '__main__':
     #                                             steps_per_epoch=len(loader),
     #                                             )
 
-    os.makedirs('trained_models/', exist_ok=True)
+    os.makedirs('trained_models/unet-vae-v2/', exist_ok=True)
 
     for epoch in range(1, n_epochs+1):
         train_vae(unet_vae, loader, optimizer, epoch=epoch, gradient_clip=gclip, log_interval=100, use_cuda=use_cuda)
@@ -130,7 +126,7 @@ if __name__ == '__main__':
 
         if epoch in save_points:
             print('Saving model')
-            torch.save(unet_vae.state_dict(), f'trained_models/unet-vae-epochs_{epoch}-lr_{lr}-bs_{batch_size}-gclip_{gclip}.pth')
+            torch.save(unet_vae.state_dict(), f'trained_models/unet-vae-v2/unet-vae-epochs_{epoch}-lr_{lr}-bs_{batch_size}-gclip_{gclip}.pth')
 
-    torch.save(unet_vae.state_dict(), f'trained_models/unet-vae-epochs_{n_epochs}-lr_{lr}-bs_{batch_size}-gclip_{gclip}.pth')
+    torch.save(unet_vae.state_dict(), f'trained_models/unet-vae-v2/unet-vae-epochs_{n_epochs}-lr_{lr}-bs_{batch_size}-gclip_{gclip}.pth')
 
